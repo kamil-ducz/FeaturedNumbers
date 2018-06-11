2d9a70d669dccc0b56e666e42e8d7a22c93e15b6
warlord89 <kamil.duczakowski@futurelife.com.pl>
2018-06-11 18:21:31 +0200

    initial commit

---
+++ FeaturedNumbers/FeaturedNumbers/Program.cs
@@ -0,0 +1,45 @@
+﻿//Make a function that receives a value and outputs the smallest higher number than
+//the given value and this number belongs to a set of positive integer that have the
+//following properties
+//-their digit occur only once
+//-is odd
+//-is multiple of three
+//for example:
+//(12) -> (15)
+//(99) -> (105)
+//(9999999999999999999) -> too big
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FeaturedNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input number:");
+            string value = Console.ReadLine();
+            if(int.TryParse(value, out int result))
+            {
+                try
+                {
+                    int finalValue = MathHelper.GetNextFeaturedNumber(int.Parse(value));
+                    Console.WriteLine(finalValue);
+                }
+                catch
+                {
+                    throw new Exception();
+                }
+                finally
+                {
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
